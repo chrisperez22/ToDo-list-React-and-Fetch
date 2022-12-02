a73f534067e43582d28e2f1ab20417d884a6dd1d
chrisperez22 <chrisperez0531@gmail.com>
2022-12-02 00:50:39 +0000

    my first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,152 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+let globalList = [];
+let index = -1;
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [, setCount] = useState(0);
+
+  function handleRemove(id) {
+    if (id === 0 && globalList.length === 1) {
+      alert("Api does not support deleting the last item");
+    } else {
+      globalList.splice(id, 1);
+
+      UpdateData();
+
+      // force a re-render:
+      setCount((c) => c + 1);
+
+      alert("Delete was successful");
+    }
+  }
+
+  function addTodo() {
+    if (inputValue === "") {
+      alert("Please enter a valid task");
+    } else {
+      index += 1;
+
+      let newItem = {
+        done: false,
+        label: inputValue,
+      };
+
+      globalList.push(newItem);
+
+      //now push the value to the web api
+      UpdateData();
+
+      setInputValue("");
+    }
+  }
+
+  function UpdateData() {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/DSBT", {
+      method: "PUT",
+      body: JSON.stringify(globalList),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then((data) => {
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+        RefreshData();
+      })
+      .catch((error) => {
+        //error handling
+        console.log(error);
+      });
+  }
+
+  function keyPress(e) {
+    if (e.keyCode === 13) {
+      addTodo();
+    }
+  }
+
+  function RefreshData() {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/DSBT", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        // Read the response as json.
+        return response.json();
+      })
+      .then((responseAsJson) => {
+        for (let i = 0; i < responseAsJson.length; i++) {
+          console.log(responseAsJson[i]);
+          globalList.push(responseAsJson[i]);
+        }
+
+        //refresh screen
+        setCount((c) => c + 1);
+      })
+      .catch((error) => {
+        console.log("Looks like there was a problem: \n", error);
+      });
+  }
+
+  useEffect(() => {
+    RefreshData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="main-div">
+        <p>To Do's</p>
+        <div className="input-div">
+          <div className="row">
+            <div className="col-10">
+              <input
+                type="text"
+                onKeyDown={(e) => keyPress(e)}
+                onChange={(e) => setInputValue(e.target.value)}
+                value={inputValue}
+              ></input>
+            </div>
+            <div className="col-1">
+              <button id="my_add_btn" onClick={() => addTodo()}>
+                Task
+              </button>
+            </div>
+          </div>
+          <ul id="my-list" className="todo-container">
+            {globalList.length > 0 ? (
+              globalList.map((item, index) => (
+                <li key={index}>
+                  <span>{item.label}</span>
+                  <button
+                    id="delete-btn"
+                    type="button"
+                    onClick={() => handleRemove(index)}
+                  >
+                    X
+                  </button>
+                </li>
+              ))
+            ) : (
+              <h3>No tasks, Add a task</h3>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
